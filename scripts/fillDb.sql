cf9399b68ec732db00f33ba3d1ce332a20f0553f
Andrey <Kuksa>
2021-04-05 19:41:53 +0300

    Changed sql

--- scripts/fillDb.sql
+++ scripts/fillDb.sql
@@ -8,14 +8,11 @@ VALUES (1, 'Action film'),
 
 
 INSERT INTO users
-VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMIN', 50),
-       (2, 'test_user', '25d55ad283aa400af464c76d713c07ad', 'ADMIN', 50),
-       (3, 'user0', 'e80b5017098950fc58aad83c8c14978e', 'ADMIN', 50),
-       (4, 'user1', 'e80b5017098950fc58aad83c8c14978e', 'ADMIN', 50),
-       (5, 'user2', 'e80b5017098950fc58aad83c8c14978e', 'ADMIN', 50),
-       (6, 'user3', 'e80b5017098950fc58aad83c8c14978e', 'ADMIN', 50),
-       (7, 'user4', 'e80b5017098950fc58aad83c8c14978e', 'ADMIN', 50),
-       (8, 'user5', 'e80b5017098950fc58aad83c8c14978e', 'ADMIN', 50),
-       (9, 'user6', 'e80b5017098950fc58aad83c8c14978e', 'ADMIN', 50);
+VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMIN', 50),
+       ('test', '21232f297a57a5a743894a0e4a801fc3', 'USER', 50),
+       ('test_1', '21232f297a57a5a743894a0e4a801fc3', 'USER', 50),
+       ('test_2', '21232f297a57a5a743894a0e4a801fc3', 'USER', 50),
+       ('test_3', '21232f297a57a5a743894a0e4a801fc3', 'USER', 50);
+
 
 
