a36be91add0ac0280c2444c8a91daa87bc422b72
Andrey <Kuksa>
2021-04-10 16:47:27 +0300

    solve problem with loading image

--- src/main/java/com/epam/web/parser/ImageFormParser.java
+++ src/main/java/com/epam/web/parser/ImageFormParser.java
@@ -3,10 +3,13 @@ package com.epam.web.parser;
 import org.apache.commons.fileupload.FileItem;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class ImageFormParser implements FormParser {
-    private static final String IMAGE_PATH =
-            "D:/Java/Projects/epam-final_project/src/main/webapp/static/img/movies/";
+    private static final String IMAGE_PATH = "../webapps/Epam_Final_Project_war/static/img/movies/";
+    private static final String BACKUP_PATH = "D:/Java/Projects/epam-final_project/src/main/webapp/static/img/movies/";
     private static final String SAVE_PATH = "static/img/movies/";
 
     @Override
@@ -14,6 +17,9 @@ public class ImageFormParser implements FormParser {
         String imagePath = IMAGE_PATH + item.getName();
         File imageSaveFile = new File(imagePath);
         item.write(imageSaveFile);
+        Path originalPath = Path.of(imagePath);
+        Path copied = Path.of(BACKUP_PATH + item.getName());
+        Files.copy(originalPath, copied);
         String savePath = SAVE_PATH + item.getName();
         return ParseResult.imagePath(savePath);
     }
